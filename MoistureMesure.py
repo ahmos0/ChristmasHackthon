9ea481a9c3e33895b64975245700089a623d83dc
ahmos0 <kamaboko_0228@icloud.com>
2022-12-13 08:40:16 +0000

    湿潤センサーが反応できるようにした

---
+++ MoistureMesure.py
@@ -0,0 +1,35 @@
+# -*- coding:utf-8 -*-
+import time
+import spidev
+
+Vref = 3.334 
+
+spi = spidev.SpiDev()
+
+spi.open(0, 0) 
+spi.max_speed_hz = 100000  
+
+
+def readAdc(channel):
+    adc = spi.xfer2([1, (8 + channel) << 4, 200])
+    data = ((adc[1] & 3) << 8) + adc[2]
+    return data
+
+
+def convertVolts(data, vref):
+    mois = (data * vref) / float(1023)
+    return mois
+
+
+
+if __name__ == '__main__':
+    try:
+        while True:
+            data = readAdc(channel=0)
+            mois = convertVolts(data, Vref)
+            print("CH0 mois: {:.2f}".format(mois))
+
+            time.sleep(2)
+
+    except KeyboardInterrupt:
+        spi.close()
