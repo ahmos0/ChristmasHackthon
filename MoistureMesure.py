db423d5b0ad6cb4ec1378232ebb6108596a0ecb3
ahmos0 <kamaboko_0228@icloud.com>
2022-12-17 08:33:29 +0000

    水位を検知してLINEに通知できるようになりました

--- MoistureMesure.py
+++ MoistureMesure.py
@@ -1,6 +1,17 @@
 # -*- coding:utf-8 -*-
 import time
 import spidev
+import requests
+
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
+
+# 発行されたトークンID
+ACCESS_TOKEN =os.environ['ACCESS_TOKEN']
+STR_NOTICE_MESSAGE = 'お湯張りができました'
+
 
 Vref = 3.334 
 
@@ -20,6 +31,12 @@ def convertVolts(data, vref):
     mois = (data * vref) / float(1023)
     return mois
 
+def fnc_line_notify():
+    line_notify_api  =  "https://notify-api.line.me/api/notify"
+    headers = {"Authorization": f"Bearer {ACCESS_TOKEN}"}
+    data = {'message': f'{STR_NOTICE_MESSAGE}'}
+    requests.post(line_notify_api, headers=headers, data=data)
+    return
 
 
 if __name__ == '__main__':
@@ -28,7 +45,9 @@ if __name__ == '__main__':
             data = readAdc(channel=0)
             mois = convertVolts(data, Vref)
             print("CH0 mois: {:.2f}".format(mois))
-
+            if mois < 1.8:
+                fnc_line_notify()
+                break
             time.sleep(2)
 
     except KeyboardInterrupt:
